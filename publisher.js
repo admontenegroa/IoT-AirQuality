51d05854b2e70d1e8d339731d8fb29d165d56632
admontenegro <admontenegroa@gmail.com>
2019-09-13 12:21:00 -0500

    MQTT Protocol - Publisher 0.1

--- publisher.js
+++ publisher.js
@@ -3,7 +3,7 @@ var client  = mqtt.connect('mqtt://test.mosquitto.org')
 
 AirQuality= {"Air Quality":{"CO":"200ppm"}, "Geolocation":{"Latitude":"4.6097100°","Longitude":"-74.0817500°"}};
 client.on('connect', function () {
-  setInterval(function(){client.publish('CarbonMonoxide', AirQuality)},1000)
+  setInterval(function(){client.publish('AirQuality', AirQuality)},1000)
 })
 
 

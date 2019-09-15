fea1ea7db0d9b50b257a2cb6b7ed66e2efa7cf2e
admontenegro <admontenegroa@gmail.com>
2019-09-14 21:26:17 -0500

    MQTT Protocol - Subscribe 0.3

--- subscriber.js
+++ subscriber.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('http://broker.mqtt-dashboard.com/')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
 
 client.on('connect', function () {
   client.subscribe('AirQuality/#');
